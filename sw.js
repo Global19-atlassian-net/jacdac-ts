681df4a9f1acf1c505a9e276fbe3575062fe2972
pelikhan <pelikhan@users.noreply.github.com>
2020-10-15 10:25:57 +0000

    deploy: 0f6d03fa255364849bc04a662baa2d9c75f74578

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9f34e3598477d2c518d5.js"
+    "url": "webpack-runtime-4ba7f7842da36fff2fc4.js"
   },
   {
     "url": "styles.273c79667365f697bf1c.css"
@@ -45,11 +45,11 @@ self.__precacheManifest = [
     "url": "f65a48b9-047a011c6021491af099.js"
   },
   {
-    "url": "app-a8f73aa168ad5c557075.js"
+    "url": "app-ea2e0c4fd0334221122c.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ba4617956298e3274d28f7eafbfcb464"
+    "revision": "ca68fd7fc92da2a962432e4cc6df9c42"
   },
   {
     "url": "static/webfonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -69,7 +69,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "b9f698baa8cdc1d3d53b9928fe3767d3"
+    "revision": "9293223fcdc74a947b12d7b448bee85d"
   },
   {
     "url": "polyfill-557a1d3e3c5faa9f0e99.js"
@@ -168,7 +168,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/jacdac-ts/app-a8f73aa168ad5c557075.js`))) {
+  if (!resources || !(await caches.match(`/jacdac-ts/app-ea2e0c4fd0334221122c.js`))) {
     return await fetch(event.request)
   }
 
