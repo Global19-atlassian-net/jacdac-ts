0d438433560df33291404ca20b9f3740505cf517
pelikhan <pelikhan@users.noreply.github.com>
2021-01-08 18:14:49 +0000

    deploy: 17634cead1c928ac66f10530585e689d2a6baf8c

--- sw.js
+++ sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "framework-42603c295472826cbd1a.js"
   },
   {
-    "url": "app-044942441b052dfb57de.js"
+    "url": "app-2d7839bf085fb678fee8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "09abc484d2a2ed96e58f4ee19d4d48f4"
+    "revision": "81f714af63e1dd5dcd590413f50b3a76"
   },
   {
     "url": "static/webfonts/s/roboto/v20/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -63,7 +63,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/2214083594.json",
-    "revision": "62d29d918ab006d366cde9fd5c937350"
+    "revision": "1f070e88dd491e6a84e96e6047fadfc7"
   },
   {
     "url": "page-data/sq/d/2270080012.json",
@@ -75,7 +75,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/sq/d/2744294623.json",
-    "revision": "a77e7f0122269e1aaf2f1eba2da94c5f"
+    "revision": "8a84b55f2f75de9af7139864aa67b406"
   },
   {
     "url": "page-data/sq/d/3159585216.json",
@@ -91,7 +91,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "4e8ea99636b6d67eb2a2b6391cd06a7a"
+    "revision": "cbb4f1e0cedf052d9258445a593600bf"
   },
   {
     "url": "polyfill-e54661374584660e3f5b.js"
@@ -557,7 +557,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/jacdac-ts/app-044942441b052dfb57de.js`))) {
+  if (!resources || !(await caches.match(`/jacdac-ts/app-2d7839bf085fb678fee8.js`))) {
     return await fetch(event.request)
   }
 
